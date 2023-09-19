801af02cb341bf25e883ae3f2eeefdc403efa184
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-19 22:42:46 +0330

    Add files via upload

---
+++ WindowsFormsApp82/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp82
+{
+    public class Tarkish
+    {
+        public int x
+        {
+            get;
+            set;
+        }
+        public int n
+        {
+            get;
+            set;
+        }
+
+        public int Composition(mydelegate t)
+        {
+            return (t(n) / t(x) * t(n - x));
+        }
+    }
+}
---
+++ WindowsFormsApp82/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp82
+{
+    class Class2
+    {
+        public delegate double mydelegate(int x);
+    }
+}
